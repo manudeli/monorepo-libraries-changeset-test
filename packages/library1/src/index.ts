d721dff16e4187ff1f6b914a53134d6a25340dc2
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-04-15 16:48:23 +0900

    feat(library1): add addNumber

--- packages/library1/src/index.ts
+++ packages/library1/src/index.ts
@@ -2,3 +2,5 @@ export const add =
   <Str1 extends string>(str1: Str1) =>
   <Str2 extends string>(str2: Str2) =>
     `${str1}${str2}` as const
+
+export const addNumber = (num1: number) => (num2: number) => num1 + num2
